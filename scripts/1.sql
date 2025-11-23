c545a3e2eca48b9c3f76ff8696051f3347ddf99d
enzogabryelbezerra2018-ui <enzogabryelbezerra2018@gmail.com>
2025-11-23 20:49:02 -0300

    Create 1.sql

---
+++ scripts/1.sql
@@ -0,0 +1,7 @@
+INSERT INTO usuarios (nome, senha)
+VALUES
+('Gabriel', '929&;49:938:'),
+('Ana', 12345),
+('Maria', 'kkejv&&3(&3&30'),
+('MY DATA BASE OFICIAL', 'NULLNULLNULL'),
+('PEDRO BUNDASECA silva', 09949398),
