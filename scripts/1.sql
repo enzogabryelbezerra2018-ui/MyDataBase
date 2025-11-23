2319b5233062e78b046b046de9cb03b4d78985bc
enzogabryelbezerra2018-ui <enzogabryelbezerra2018@gmail.com>
2025-11-23 20:53:33 -0300

    Update 1.sql

--- scripts/1.sql
+++ scripts/1.sql
@@ -5,3 +5,7 @@ VALUES
 ('Maria', 'kkejv&&3(&3&30'),
 ('MY DATA BASE OFICIAL', 'NULLNULLNULL'),
 ('PEDRO BUNDASECA silva', 09949398),
+('Hermenegildo Pé-de-Cabra', '&kskfkrjis93859'),
+('José Carrapato', '9838(&338(&/:8'),
+('Severino Cocozinho', 938594939449494959392949493939593939459483949493839494939339494838383),
+('Raimunda Pé-de-Galinha', 'RAIMUNDA1234')
